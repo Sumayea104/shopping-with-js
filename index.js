085ee573382ea04a631e50d76db7f8e94a4ee82a
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-18 19:47:57 +0600

    first card

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ document.getElementById('first-card').addEventListener('click',function(){
     const productPrice = document.getElementById('first-price').innerText;
     const productQuantity = document.getElementById('first-quantity').innerText;
     // console.log(productName, productPrice, productQuantity);
+    const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
+    // console.log(priceTotal);
     const container = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML=`
     <td>${1}</td>
-    
+    <td>${productName}</td>
+    <td>${productPrice}</td>
+    <td>${productQuantity}</td>
+    <td>${priceTotal}</td>
     `;
     container.appendChild(tr)
 })
