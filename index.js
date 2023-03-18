740191d2f2a64278b66fc44fc2bed99e4dd036ed
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-18 19:59:23 +0600

    function converted

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // console.log('clicked');
-document.getElementById('first-card').addEventListener('click',function(){
+document.getElementById('first-card').addEventListener('click', function () {
     // console.log('btn clicked')
 
-    // get data from html -using id
+    // step -01 get data from html -using id
     const productName = document.getElementById('first-name').innerText;
     // console.log(firstElement);
     const productPrice = document.getElementById('first-price').innerText;
@@ -10,14 +10,21 @@ document.getElementById('first-card').addEventListener('click',function(){
     // console.log(productName, productPrice, productQuantity);
     const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
     // console.log(priceTotal);
+
+    //  step -02 show the data
+    displayData(productName, productPrice, productQuantity, priceTotal);
+})
+
+function displayData(productName, productPrice, productQuantity, priceTotal) {
+    console.log('called')
     const container = document.getElementById('table-container');
     const tr = document.createElement('tr');
-    tr.innerHTML=`
-    <td>${1}</td>
-    <td>${productName}</td>
-    <td>${productPrice}</td>
-    <td>${productQuantity}</td>
-    <td>${priceTotal}</td>
-    `;
+    tr.innerHTML = `
+<td>${1}</td>
+<td>${productName}</td>
+<td>${productPrice}</td>
+<td>${productQuantity}</td>
+<td>${priceTotal}</td>
+`;
     container.appendChild(tr)
-})
+}
