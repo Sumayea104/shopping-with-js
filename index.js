d8c828abab295ba1ab9af2f2dac77313b6aecfdd
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-18 19:22:49 +0600

    table

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log('clicked');
+document.getElementById('first-card').addEventListener('click',function(){
+    // console.log('btn clicked')
+    const productName = document.getElementById('first-name').innerText;
+    // console.log(firstElement);
+    const productPrice = document.getElementById('first-price').innerText;
+    const productQuantity = document.getElementById('first-quantity').innerText;
+    // console.log(productName, productPrice, productQuantity);
+
+
+})
